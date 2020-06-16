ca14d45b7f474aea5b286ad77bab1f504aa8c99f
Robert Laws <roblaws@me.com>
2020-06-16 11:41:44 +0300

    setup front end environment

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <h2>Website Title</h2>
+      <nav>
+        <button>Services</button>
+        <button>About Us</button>
+        <button>Contact Us</button>
+        <button>Our Mission</button>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
