23d0e71d309cfc8a7024583eb021bf194c7f3013
Robert Laws <roblaws@me.com>
2020-06-27 11:23:38 +0300

    add modal for deleting place

--- client/src/places/components/PlaceItem.js
+++ client/src/places/components/PlaceItem.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../shared/components/FormElements/Button';
+import Modal from '../../shared/components/UIElements/Modal';
 
 const PlaceItem = ({ place }) => {
+  const [confirmModal, setConfirmModal] = useState(false);
+
+  const deleteWarningHandler = () => {
+    setConfirmModal(true);
+  };
+
+  const cancelDeleteHandler = () => {
+    setConfirmModal(false);
+  };
+
+  const confirmDeleteHandler = () => {
+    setConfirmModal(false);
+    console.log('deleting...');
+  };
+
   const {
     id,
     imageUrl,
@@ -13,21 +29,37 @@ const PlaceItem = ({ place }) => {
   } = place;
 
   return (
-    <li className='place-item'>
-      <div className='image'>
-        <img src={imageUrl} alt={title} />
-        <div className='info'>
-          <h2>{title}</h2>
-          <h3>{address}</h3>
-          <p>{description}</p>
+    <>
+      <Modal
+        show={confirmModal}
+        cancel={cancelDeleteHandler}
+        header='Are you sure?'
+        footerClass='modal-buttons'
+        footer={
+          <>
+            <Button onClick={cancelDeleteHandler}>Cancel</Button>
+            <Button onClick={confirmDeleteHandler}>Delete</Button>
+          </>
+        }
+      >
+        <p>Do you want to proceed and delete this place?</p>
+      </Modal>
+      <li className='place-item'>
+        <div className='image'>
+          <img src={imageUrl} alt={title} />
+          <div className='info'>
+            <h2>{title}</h2>
+            <h3>{address}</h3>
+            <p>{description}</p>
+          </div>
         </div>
-      </div>
-      <div className='actions'>
-        <Button>View on Map</Button>
-        <Button to={`/places/${id}`}>Edit</Button>
-        <Button>Delete</Button>
-      </div>
-    </li>
+        <div className='actions'>
+          <Button>View on Map</Button>
+          <Button to={`/places/${id}`}>Edit</Button>
+          <Button onClick={deleteWarningHandler}>Delete</Button>
+        </div>
+      </li>
+    </>
   );
 };
 
