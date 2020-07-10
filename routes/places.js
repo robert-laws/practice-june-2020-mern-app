2762ca4d09d5ce7fdece078cac9e2e30bdb9f58e
Robert Laws <roblaws@me.com>
2020-07-10 15:30:40 +0300

    add custom error class

--- routes/places.js
+++ routes/places.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const HttpError = require('../models/http-error');
 
 const TEMP_PLACES = [
   {
@@ -38,9 +39,7 @@ router.get('/:id', (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error('Could not find place for the provided id.');
-    error.code = 404;
-    throw error;
+    throw new HttpError('Could not find place for the provided id.', 404);
   }
 
   res.json({ place });
@@ -54,9 +53,9 @@ router.get('/user/:id', (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the provided user id');
-    error.code = 404;
-    return next(error);
+    return next(
+      new HttpError('Could not find a place for the provided user id', 404)
+    );
   }
 
   res.json({ place });
